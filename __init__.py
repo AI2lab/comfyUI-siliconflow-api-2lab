e9942536566323b5da6c3ea11a2869c74b96b13d
liguo <bakkhos@gmail.com>
2024-06-26 09:54:17 +0800

    config.json

--- __init__.py
+++ __init__.py
@@ -1,7 +1,14 @@
+import os
+import shutil
 
+from .constants import project_root
+
+config_file_path = os.path.join(project_root, "config.json")
+if not os.path.exists(config_file_path):
+    config_template_file_path = os.path.join(project_root, "config_template.json")
+    shutil.copy(config_template_file_path, config_file_path)
 
 from .siliconflow import FreeChat,PaidChat
-
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
